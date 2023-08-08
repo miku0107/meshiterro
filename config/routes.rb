6e4ca8dd58706a8ddf30e6266fbedba49b64ced6
miku0107 <138674231+miku0107@users.noreply.github.com>
2023-08-08 20:29:32 +0000

    login/out_after_rootpath

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
     
+  get 'homes/about' => 'homes#about'
+  
   devise_for :users
   
   root to: 'homes#top'
+  
     
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
